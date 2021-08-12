aa37b827ffb2d4cb536f9ad0f6d7900800de8754
ankitaugale23 <ankitaugale23@gmail.com>
2021-08-12 23:57:04 +0530

    complete google map api connection and map component layout

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,7 +1,33 @@
 import react from "react";
+import GoogleMapReact from "google-map-react";
+import {Paper, Typography, useMediaQuery} from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Maps = ()=> {
-    return <h1>Maps</h1>
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width: 600px)');
+
+    const coordinates = {lat: 0, lng: 0};
+    return (
+        <div className={classes. mapContainer}>
+            <GoogleMapReact    /*main component which will provide map from google maps api*/
+                bootstrapURLKeys={{key: 'AIzaSyDBRxOlgkPQv-tQe1wydXs7GpgMt7RJHsc' }}  /*google map api key*/
+                defaultCenter={coordinates}  /*default centered place's co ordinates*/
+                center={coordinates}
+                defaultZoom={2}
+                margin={[50,50,50,50]} /*four margins*/
+                options={''}
+                onChange={''}
+                onChildClick={''}   /*upadte when user will select any other resturant*/
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
 }
 
 export default Maps;
---
+++ src/components/Maps/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '89vh', width: '205%',
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
